5f78c0d4b2e604408750bbd19b4c67c4cfa57a79
umut karaytu <umutkaraytu20@gmail.com>
2025-10-20 19:12:36 +0300

    s

--- components/StarRating.jsx
+++ components/StarRating.jsx
@@ -3,25 +3,34 @@
 import React from "react";
 import { FiStar } from "react-icons/fi";
 
-const StarRating = ({ rating = 0, onRatingChange = null }) => {
+/**
+ * StarRating Component
+ * @param {number} rating - 0 ile 5 arası puan (0.5'lik dilimlere destek verir)
+ * @param {function|null} onRatingChange - Yıldız tıklanınca çalışacak fonksiyon (isteğe bağlı)
+ * @param {number} size - Yıldız boyutu (varsayılan 24px)
+ */
+const StarRating = ({ rating = 0, onRatingChange = null, size = 24 }) => {
   const handleClick = (value) => {
-    if (onRatingChange) {
-      onRatingChange(value);
-    }
+    if (onRatingChange) onRatingChange(value);
   };
 
   return (
-    <div className="flex gap-1 relative">
+    <div className="flex gap-1">
       {[1, 2, 3, 4, 5].map((star) => {
         const isFull = rating >= star;
         const isHalf = rating >= star - 0.5 && rating < star;
+
         return (
           <div
             key={star}
-            className="relative w-6 h-6 cursor-pointer"
+            className="relative cursor-pointer"
+            style={{ width: size, height: size }}
           >
             {/* Boş yıldız */}
-            <FiStar className="w-6 h-6 text-gray-300 absolute top-0 left-0" />
+            <FiStar
+              className="absolute top-0 left-0 text-gray-300"
+              style={{ width: size, height: size }}
+            />
 
             {/* Dolu veya yarım yıldız */}
             <div
@@ -29,16 +38,19 @@ const StarRating = ({ rating = 0, onRatingChange = null }) => {
                 isFull ? "w-full" : isHalf ? "w-1/2" : "w-0"
               }`}
             >
-              <FiStar className="w-6 h-6 text-yellow-400 fill-yellow-400" />
+              <FiStar
+                className="text-yellow-400 fill-yellow-400"
+                style={{ width: size, height: size }}
+              />
             </div>
 
-            {/* Sol yarıya tıklama (0.5 puan) */}
+            {/* Sol yarı tıklama */}
             <div
               className="absolute top-0 left-0 h-full w-1/2 z-10"
               onClick={() => handleClick(star - 0.5)}
             />
 
-            {/* Sağ yarıya tıklama (1 puan) */}
+            {/* Sağ yarı tıklama */}
             <div
               className="absolute top-0 right-0 h-full w-1/2 z-10"
               onClick={() => handleClick(star)}
